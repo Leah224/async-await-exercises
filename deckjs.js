32791706c6028541d106bc7bf6e94a4581b5f79b
Leah224 <Leahc9799@gmail.com>
2025-09-18 11:23:54 -0400

    Update JS to draw two cards from same deck

--- deckjs.js
+++ deckjs.js
@@ -8,3 +8,27 @@ fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
         console.log(`${card.value} of ${card.suit}`); // e.g. "QUEEN of HEARTS"
     })
     .catch(err => console.error(err));
+
+// Step 2: Draw two cards from the SAME deck
+fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
+  .then(res => res.json())
+  .then(data => {
+    const deckId = data.deck_id;
+
+    // Draw the first card
+    return fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+      .then(res => res.json())
+      .then(firstDraw => {
+        const card1 = firstDraw.cards[0];
+        console.log(`${card1.value} of ${card1.suit}`);
+
+        // Draw the second card
+        return fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+          .then(res => res.json())
+          .then(secondDraw => {
+            const card2 = secondDraw.cards[0];
+            console.log(`${card2.value} of ${card2.suit}`);
+          });
+      });
+  })
+  .catch(err => console.error(err));
